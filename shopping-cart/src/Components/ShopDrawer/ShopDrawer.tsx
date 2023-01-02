dec66021315f53fce8d8949114b275441e36716d
Cory Grewohl <corywalter.grewohl@calbaptist.edu>
2023-01-01 17:19:06 -0800

    commented out shopdrawer wip

--- shopping-cart/src/Components/ShopDrawer/ShopDrawer.tsx
+++ shopping-cart/src/Components/ShopDrawer/ShopDrawer.tsx
@@ -7,63 +7,64 @@ export const ShopDrawer = () => {
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   return (
-    <Drawer
-        anchor="left"
-        open={isDrawerOpen}
-        onClose={() => setIsDrawerOpen(false)}
-      >
-        <IconButton onClick={() => setIsDrawerOpen(false)}>
-          <CloseIcon />
-        </IconButton>
-        <Box p={2} width="250px" textAlign="center" role="presentation">
-          <Typography variant="h6">Filter</Typography>
-          <FormControl>
-            <InputLabel id="sort-dropdown">Sort By:</InputLabel>
-            <Select
-              labelId="sort-dropdown"
-              label="Sort By"
-              sx={{ minWidth: 120 }}
-              value={sortBy}
-              onChange={handleChange}
-            >
-              <MenuItem value={"lowToHigh"}>Lowest to Highest</MenuItem>
-              <MenuItem value={"highToLow"}>Highest to Lowest</MenuItem>
-              <MenuItem value={"alphabetical"}>A to Z</MenuItem>
-            </Select>
-            <FormGroup>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={selectedType.bricks}
-                    onChange={handleCheck}
-                    name="bricks"
-                  />
-                }
-                label="Bricks"
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={selectedType.plates}
-                    onChange={handleCheck}
-                    name="plates"
-                  />
-                }
-                label="Plates"
-              />
-            </FormGroup>
-            <Button
-              variant="outlined"
-              onClick={() => {
-                setIsDrawerOpen(false);
-                filterPieces(selectedType.bricks, selectedType.plates)
-              }}
-              sx={{ marginTop: "20px" }}
-            >
-              Save
-            </Button>
-          </FormControl>
-        </Box>
-      </Drawer>
+    <></>
+    // <Drawer
+    //     anchor="left"
+    //     open={isDrawerOpen}
+    //     onClose={() => setIsDrawerOpen(false)}
+    //   >
+    //     <IconButton onClick={() => setIsDrawerOpen(false)}>
+    //       <CloseIcon />
+    //     </IconButton>
+    //     <Box p={2} width="250px" textAlign="center" role="presentation">
+    //       <Typography variant="h6">Filter</Typography>
+    //       <FormControl>
+    //         <InputLabel id="sort-dropdown">Sort By:</InputLabel>
+    //         <Select
+    //           labelId="sort-dropdown"
+    //           label="Sort By"
+    //           sx={{ minWidth: 120 }}
+    //           value={sortBy}
+    //           onChange={handleChange}
+    //         >
+    //           <MenuItem value={"lowToHigh"}>Lowest to Highest</MenuItem>
+    //           <MenuItem value={"highToLow"}>Highest to Lowest</MenuItem>
+    //           <MenuItem value={"alphabetical"}>A to Z</MenuItem>
+    //         </Select>
+    //         <FormGroup>
+    //           <FormControlLabel
+    //             control={
+    //               <Checkbox
+    //                 checked={selectedType.bricks}
+    //                 onChange={handleCheck}
+    //                 name="bricks"
+    //               />
+    //             }
+    //             label="Bricks"
+    //           />
+    //           <FormControlLabel
+    //             control={
+    //               <Checkbox
+    //                 checked={selectedType.plates}
+    //                 onChange={handleCheck}
+    //                 name="plates"
+    //               />
+    //             }
+    //             label="Plates"
+    //           />
+    //         </FormGroup>
+    //         <Button
+    //           variant="outlined"
+    //           onClick={() => {
+    //             setIsDrawerOpen(false);
+    //             filterPieces(selectedType.bricks, selectedType.plates)
+    //           }}
+    //           sx={{ marginTop: "20px" }}
+    //         >
+    //           Save
+    //         </Button>
+    //       </FormControl>
+    //     </Box>
+    //   </Drawer>
   )
 }
